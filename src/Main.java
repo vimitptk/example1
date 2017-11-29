d4609d218e4cd316d18d9c2c80f972cf52980253
Riks <vimitptk@mail.ru>
2017-11-29 23:21:11 +0600

    qwe www tggggg

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("qwjgk bjbk");
+        System.out.println("qwjgk bjbk");
     }
 }
